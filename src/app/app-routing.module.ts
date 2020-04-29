7aec0b87091b06e9794b79225b0de35fb57ac7bd
VladBohdan <darekz04111995@gmail.com>
2020-04-29 13:33:23 +0300

    Dynamic Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,23 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
 import {PostsComponent} from './posts/posts.component';
+import {PostComponent} from './post/post.component';
 
 // http://localhost:4200/ -> HomeComponent
 // http://localhost:4200/about -> AboutComponent
 // http://localhost:4200/posts -> PostsComponent
+
 const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'posts', component: PostsComponent}
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'posts', component: PostsComponent},
+  {path: 'posts/:id', component: PostComponent}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
+
 }
