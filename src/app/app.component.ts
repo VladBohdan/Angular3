664b0a23c68bc17f539b0b5c5f5dc5932a8801dd
VladBohdan <darekz04111995@gmail.com>
2020-05-05 16:48:42 +0300

    Animation events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {animate, group, query, state, style, transition, trigger} from '@angular/animations';
+import {animate, group, keyframes, query, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
     selector: 'app-root',
@@ -36,8 +36,14 @@ import {animate, group, query, state, style, transition, trigger} from '@angular
 
             // void =>*
             transition(':enter', [
-                style({opacity: 0}),
-                animate('850ms ease-out')
+                animate('4s', keyframes([
+                    style({background: 'red', offset: 0}),
+                    style({background: 'black', offset: 0.2}),
+                    style({background: 'orange', offset: 0.3}),
+                    style({background: 'blue', offset: 1})
+                ])),
+                // style({opacity: 0}),
+                //  animate('850ms ease-out')
             ]),
             // * => void
             transition(':leave', [
@@ -62,5 +68,13 @@ export class AppComponent {
 
     animate() {
         this.boxState = this.boxState === 'end' ? 'start' : 'end';
+    }
+
+    animationStarted(event: AnimationEvent) {
+        console.log('animationStarted', event);
+    }
+
+    animationDone(event: AnimationEvent) {
+        console.log('animationDone', event);
     }
 }
